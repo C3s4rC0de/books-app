27725148e6c5e76f803d8345025424be2c465f1d
César Tamayo <dragontona@gmail.com>
2022-08-05 03:02:53 -0500

    Include theming

---
+++ src/theme/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#DEC700",
+    },
+    secondary: {
+      main: "#9A9A9A",
+    },
+  },
+});
