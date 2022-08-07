020155d60174949c6730dfdc5b64e67f25877880
César Tamayo <dragontona@gmail.com>
2022-08-07 06:05:04 -0500

    Tests plugin

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
   images: {
     domains: ["books.google.com"],
   },
+  webpack: (config, { webpack }) => {
+    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
+    return config;
+  },
 };
 
 module.exports = nextConfig;
